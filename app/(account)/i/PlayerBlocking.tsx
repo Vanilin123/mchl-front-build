5b6286e100dd5066797c8eda61df7c3c5d1b72d9
Siganov Ivan <siganov@mail.ru>
2024-09-09 20:49:46 +0300

    new info

---
+++ app/(account)/i/PlayerBlocking.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { FormProvider, useForm } from 'react-hook-form'
+
+import type { TypePlayerFormState } from '../../../types/player'
+import { PlayerBlockingList } from './PlayerBlockingList'
+import { PlayerForm } from './form/playerForm'
+
+export function PlayerBlocking() {
+	const methods = useForm<TypePlayerFormState>()
+
+	return (
+		<FormProvider {...methods}>
+		<PlayerBlockingList />
+		</FormProvider>
+	)
+}
---
+++ app/news/[id]/page.jsx
@@ -0,0 +1,18 @@
+
+
+import News from '../../../app/news/[id]/newsPost'
+
+
+
+
+
+
+export default function Dashboard() {
+	
+	
+	return (
+		
+		<News/>
+		
+	)
+}

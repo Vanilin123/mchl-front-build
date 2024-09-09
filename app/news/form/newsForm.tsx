6c5fef8fd7c8598082961e4108f37da6da52aa2a
Vanilin123 <149298214+Vanilin123@users.noreply.github.com>
2024-09-09 20:56:54 +0300

    Update newsForm.tsx

--- app/news/form/newsForm.tsx
+++ app/news/form/newsForm.tsx
@@ -1,7 +1,7 @@
 import { Field } from '../../../components/Fields/Field'
 import { Controller, SubmitHandler, useFormContext } from 'react-hook-form'
 // @ts-ignore
-import { Button } from '../../../../components/buttons/Button'
+import { Button } from '../../../components/buttons/Button'
 import { useProfile } from '../../../hooks/useProfile'
 import { useCreateNews } from './useCreateNews'
 import { useUpdateNews } from './useUpdateNews'

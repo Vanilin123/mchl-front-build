b635784acda4836c3fcdabc8c378876609145aed
Vanilin123 <149298214+Vanilin123@users.noreply.github.com>
2024-09-09 20:56:31 +0300

    Update documentsForm.tsx

--- app/documents/form/documentsForm.tsx
+++ app/documents/form/documentsForm.tsx
@@ -1,7 +1,7 @@
 import { Field } from '../../../components/Fields/Field'
 import { Controller, SubmitHandler, useFormContext } from 'react-hook-form'
 // @ts-ignore
-import { Button } from '../../../../components/buttons/Button'
+import { Button } from '../../../components/buttons/Button'
 import { useState } from "react";
 // @ts-ignore
 import type { TypeDocuments } from '@/types/documents.types'
